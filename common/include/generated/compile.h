af23998c0f37a48ea7fef375ff9e82a7fac919a0
Gustavo <lp_gustavo_@hotmail.com>
2012-07-29 00:29:58 -0300

    Primeiro commit

---
+++ common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 31 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#31 PREEMPT Thu Dec 29 23:12:48 PST 2011"
+#define LINUX_COMPILE_TIME "23:12:48"
+#define LINUX_COMPILE_BY "ansik"
+#define LINUX_COMPILE_HOST "ansik-desktop"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
